7c0b422c6e25da0977ae8c1fbd61307f13531e68
Bennett Nguyễn <7akk4r2821@gmail.com>
2023-03-20 20:44:37 +0700

    Remove old files

--- src/comp/scene/main_scene.py
+++
@@ -1,8 +0,0 @@
-import pygame as pg
-import src.preload.constant as const
-from src.preload.template import Scene
-
-class Game(Scene):
-    def __init__(self):
-        super().__init__()
-        self.id = const.GAME_SCENE_ID
